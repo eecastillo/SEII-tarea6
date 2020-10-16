8702a880bda4294b7f70d9f6f9afdaeb13599561
Alejandro Gudiño <gudo0399@gmail.com>
2020-10-15 22:15:20 -0500

    Definiciones de main a driver

--- source/BMI160.h
+++ source/BMI160.h
@@ -12,6 +12,27 @@
 #include "task.h"
 #include <stdio.h>
 
+// Accelerometer Registers
+#define ACC_Z_H 0x17
+#define ACC_Z_L 0x16
+#define ACC_Y_H 0x15
+#define ACC_Y_L 0x14
+#define ACC_X_H 0x13
+#define ACC_X_L 0x12
+// Gyroscope Registers
+#define GYR_Z_H 0x11
+#define GTR_Z_L 0x10
+#define GYR_Y_H 0x0F
+#define GYR_Y_L 0x0E
+#define GYR_X_H 0x0D
+#define GYR_X_L 0x0C
+#define STATUS_REG 0x1B //bit 7 data ready accelerometer, bit 6 drdy gyroscope
+#define GYR_CONF_REG 0x42
+#define ACC_CONG_REG 0x40
+#define SLAVE_ADRESS 0x68
+//Bit 5 y 4 estado de accm bit 3 y 2 estado de Gyro. Ambos normal es 01
+#define	PMU_STATUS 0x03
+#define CMD_REGISTER 0x7E //
 typedef struct
 {
 	int16_t x;
